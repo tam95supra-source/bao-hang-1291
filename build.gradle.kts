73327b4347f8b6e774daf3e5549911cb94876dc7
tam95supra-source <tam95.supra@gmail.com>
2026-08-11 20:34:17 +0700

    Add Firebase Google services plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
 
